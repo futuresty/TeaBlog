25ecea4895ae691301996639f961fe419aa0cf4e
teashown <teashown@gmail.com>
2013-05-27 20:04:41 +0800

    Initial commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+TeaBlog::Application.config.secret_token = 'c5d30d93e97af858d9d772f58a2675a191827ec74c2bf0021a821613d593f1f250ece6688c5459cb3f9bb158c83b95bda377665ea930b1bd7c509ba338e4559c'
