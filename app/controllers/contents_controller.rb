04923fb791a9d574874d2706e1f7b83b83692009
teashown <teashown@gmail.com>
2013-05-28 19:04:33 +0800

    static pages

---
+++ app/controllers/contents_controller.rb
@@ -0,0 +1,10 @@
+class ContentsController < ApplicationController
+  def home
+  end
+
+  def about
+  end
+
+  def help
+  end
+end
--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,8 @@
 TeaBlog::Application.routes.draw do
+  get "contents/home"
+  get "contents/about"
+  get "contents/help"
+
   # The priority is based upon order of creation:
   # first created -> highest priority.
 
