d0455815d2bbc3e8777446ddf20bb3ec322c1e93
teashown <teashown@gmail.com>
2013-05-30 20:07:30 +0800

    fix a bug

--- db/schema.rb
+++ db/schema.rb
@@ -20,5 +20,7 @@ ActiveRecord::Schema.define(:version => 20130530082558) do
     t.datetime "updated_at",      :null => false
     t.string   "password_digest"
   end
+
   add_index "users", ["email"], :name => "index_users_on_email", :unique => true
+
 end
