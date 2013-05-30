c3e45941ff5173cfa0f41aa045f1f358311c0d85
teashown <teashown@gmail.com>
2013-05-30 17:27:47 +0800

    complete user signup module

--- config/routes.rb
+++ config/routes.rb
@@ -1,9 +1,17 @@
 TeaBlog::Application.routes.draw do
-  get "static_pages/home"
 
-  get "static_pages/help"
+  resources :users
 
-  get "static_pages/about"
+  #get "static_pages/home"
+  #get "static_pages/help"
+  #get "static_pages/about"
+  #get "static_pages/contact"
+
+  root :to => "static_pages#home"
+  match "/help"     => "static_pages#help"
+  match "/about"    => "static_pages#about"
+  match "/contact"  => "static_pages#contact"
+  match "/signup"   => "users#new"
 
   # The priority is based upon order of creation:
   # first created -> highest priority.
